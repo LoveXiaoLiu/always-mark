290a4c0bb9b300ec46012b1cefd92612ebc6922b
grass <shuai_grass@foxmail.com>
2017-09-29 22:24:58 +0800

    解析xml

---
+++ src/backend/daemon/management/commands/parse_bookmark_xml.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: caoshuai
+# @Date:   2017-09-29 17:47:05
+# @Last Modified by:   caoshuai
+# @Last Modified time: 2017-09-29 17:47:12
+
+import os
