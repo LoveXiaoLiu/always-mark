c80bc71038f67924ad551a5a7a663e22faf2fb62
lovexiaoliu <shuai_grass@foxmail.com>
2017-12-19 15:20:41 +0800

    前端修改

--- src/web/src/indexCon.js
+++ src/web/src/indexCon.js
@@ -2,7 +2,7 @@
 * @Author: caoshuai
 * @Date:   2017-09-23 14:05:32
 * @Last Modified by:   anchen
-* @Last Modified time: 2017-11-27 19:51:04
+* @Last Modified time: 2017-12-14 21:17:47
 */
 
 var app = angular.module('myApp', []);
@@ -39,6 +39,7 @@ app.controller('myCtrl', ['$scope', '$http', function($scope, $http){
         method : 'GET',
     }).success(function (response, header, config, status) {
         console.log(response)
+        $scope.labs = response.result;
     })
 
     $scope.colorr = {
