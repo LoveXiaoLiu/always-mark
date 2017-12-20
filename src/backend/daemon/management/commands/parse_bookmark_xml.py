53375c826ddf08da8f9a861c97b3d4977f5639b6
grass <shuai_grass@foxmail.com>
2017-12-20 10:19:34 +0800

    modify parse html

--- src/backend/daemon/management/commands/parse_bookmark_xml.py
+++ src/backend/daemon/management/commands/parse_bookmark_xml.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @Author: caoshuai
-# @Date:   2017-09-29 17:47:05
-# @Last Modified by:   caoshuai
-# @Last Modified time: 2017-09-30 15:53:35
 
-from BeautifulSoup import BeautifulSoup
-from django.core.management.base import BaseCommand
+import os
 import traceback
+from bs4 import BeautifulSoup
+from logging import getLogger
+from optparse import make_option
+from django.core.management.base import BaseCommand
+
+logger = getLogger("default")
+
+PARSE_ENGINE = "html5lib"
 
 class Command(BaseCommand):
     """parse html"""
@@ -18,69 +22,45 @@ class Command(BaseCommand):
 
     option_list = BaseCommand.option_list + (
             make_option('--htmlfile', dest='htmlfile'),
+            make_option('--pid-file', dest='pid_file'),
         )
     
     def handle(self, *urls, **options):
+        logger.info("daemon parse_bookmark_xml starting......")
+
         htmlfile = options.get('htmlfile')
+        pid_file = options.get('pid_file')
+
+        if pid_file:
+            with open(pid_file, 'a') as f:
+                f.write('%d\n', % os.getpid())
 
         if htmlfile:
             run(htmlfile)
         else:
             print "htmlfile is none"
 
-
 def run(htmlfile):
-    data_str = ''
+    logger.info("start to parse html file ......")
+    bs_obj = None
+    with open(htmlfile, 'r') as fhandle:
+        bs_obj = BeautifulSoup(fhandle, PARSE_ENGINE)
+
+    if bs_obj:
+        head = bs_obj.head
+        body = bs_obj.body
+
+        dl = body.dl
+
+        parse_dl(dl.children)
+
+        parse_dt()
+
+    logger.info("end to parse html file ......")
+
+def parse_dl(dl_iter):
     try:
-        ff = open(htmlfile)
-        data_str = ff.read()
-    except:
-        print traceback.format_exc()
-    finally:
-        ff.close()
-
-    data_bs = remove_other_and_beautiful(data_str)
-    parse_data_dict = parse_html_to_dict(data_bs)
-
-    save_to_database(parse_data_dict)
-
-
-def remove_other_and_beautiful(data_str):
-    ret_dict = dict()
-
-    data_bs = BeautifulSoup(data_str)
-    lasttime_tag = None
-    lasttime_key = None
-    for data in data_bs:
-        try:
-            tag = data.name
-            text = data.string
-
-            if 'h3' == tag:
-                if lasttime_tag == tag:
-                    continue
-                
-                if u'书签栏' == text:
-                    continue
-
-                lasttime_key = text
-
-                if not ret_dict.has_key(text):
-                    ret_dict[text] = list()
-            elif 'a' == tag:
-                tmp_dict = dict()
-
-                try:
-                    tmp_dict['name'] = text
-                    tmp_dict['url'] = data.get('href')
-                except:
-                    pass
-
-                ret_dict.get(lasttime_key, list()).append(tmp_dict)
-
-            lasttime_tag = tag
-
-        except Exception, e:
-            # print traceback.format_exc()
-            continue
-    return ret_dict
+        while True:
+            
+    except StopIteration:
+        logger.info("iteration end")
