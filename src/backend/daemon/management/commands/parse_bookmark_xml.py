d937c5b82c480701e27a5e574cf547c48ad2b938
grass <shuai_grass@foxmail.com>
2017-12-21 17:33:23 +0800

    small bug

--- src/backend/daemon/management/commands/parse_bookmark_xml.py
+++ src/backend/daemon/management/commands/parse_bookmark_xml.py
@@ -112,7 +112,7 @@ def parse_dl(dl_obj):
                 # 返回两种类型，list or dict
                 dt_data = parse_dt(nt)
                 if isinstance(dt_data, list):
-                    print dt_data
+                    print dt_data[0]["href"]
                     dl_ret[ROOT_LIST_KEY] = dl_ret[ROOT_LIST_KEY] + dt_data
 
                 elif isinstance(dt_data, dict):
