541fd8997ec98a199905136ff90bf044841b6e42
grass <shuai_grass@foxmail.com>
2017-12-25 21:26:11 +0800

    small bug

--- src/web/src/indexCon.js
+++ src/web/src/indexCon.js
@@ -2,10 +2,10 @@
 * @Author: caoshuai
 * @Date:   2017-09-23 14:05:32
 * @Last Modified by:   anchen
-* @Last Modified time: 2017-12-25 21:22:56
+* @Last Modified time: 2017-12-25 21:23:58
 */
 
-var app = angular.module('myApp', ['ui.bootstrap']);
+var app = angular.module('myApp', []);
 
 app.controller('myCtrl', ['$scope', '$http', '$modal', function($scope, $http, $modal){
 
