b7f0b2ca2a07586dad2825edcbce552f4892927c
grass <shuai_grass@foxmail.com>
2017-12-25 21:03:56 +0800

    small bug

--- src/web/src/indexCon.js
+++ src/web/src/indexCon.js
@@ -2,7 +2,7 @@
 * @Author: caoshuai
 * @Date:   2017-09-23 14:05:32
 * @Last Modified by:   anchen
-* @Last Modified time: 2017-12-25 21:02:35
+* @Last Modified time: 2017-12-25 21:03:52
 */
 
 var app = angular.module('myApp', []);
@@ -24,6 +24,8 @@ app.controller('myCtrl', ['$scope', '$http', function($scope, $http){
 
 
     $scope.getTagUrls = function (tag) {
+        $scope.active_tag = tag;
+
         var geturlurl = '/get_urls/'+ tag + '/'
 
         $http({
