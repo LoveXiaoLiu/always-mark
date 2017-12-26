cd211e75dafde9fdb377546705bd72d691552c3d
grass <shuai_grass@foxmail.com>
2017-12-26 21:29:00 +0800

    small bug

--- src/web/src/indexCon.js
+++ src/web/src/indexCon.js
@@ -2,7 +2,7 @@
 * @Author: caoshuai
 * @Date:   2017-09-23 14:05:32
 * @Last Modified by:   anchen
-* @Last Modified time: 2017-12-26 21:28:12
+* @Last Modified time: 2017-12-26 21:28:56
 */
 
 var app = angular.module('myApp', ['ui.bootstrap']);
@@ -56,5 +56,5 @@ app.controller('myCtrl', ['$scope', '$http', '$modal', function($scope, $http, $
 }]);
 
 app.controller('addmarksController', ['$scope', '$modalInstance', '$http', function($scope, $modalInstance, $http){
-    
+    console.log("==================");
 }]);
