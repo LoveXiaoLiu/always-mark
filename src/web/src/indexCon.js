4adb3e96d42158ee44d863c0b2ab7eb3c5c0c197
grass <shuai_grass@foxmail.com>
2017-12-28 17:14:31 +0800

    链接样式修改

--- src/web/src/indexCon.js
+++ src/web/src/indexCon.js
@@ -2,16 +2,25 @@
 * @Author: caoshuai
 * @Date:   2017-09-23 14:05:32
 * @Last Modified by:   anchen
-* @Last Modified time: 2017-12-28 16:37:13
+* @Last Modified time: 2017-12-28 17:14:27
 */
 
 var app = angular.module('myApp', ['ui.bootstrap']);
 
 app.controller('myCtrl', ['$scope', '$http', '$modal', function($scope, $http, $modal){
-    $scope.labs = ["常用", "工具", "设备", "安全", "工具使用教程", "测试", "elasticsearch", "网络", "XSS平台", "uml", "未", "学习", "论坛", "require.js", "渗透工具", "node", "RFS", "jenkins", "架", "AngularJS", "提权", "py", "数据库", "工作", "git", "web", "虚拟机", "lnx", "博客相关"];
-    $scope.marks = [{create_date: "2017-12-21T20:48:05Z", name: "百度", url: "http://www.baidu.com", description: "0", tag_id: 23},{create_date: "2017-12-21T20:48:26Z", name: "谷歌", url: "http://www.google.com", description: "0", tag_id: 23},{create_date: "2017-12-21T20:48:44Z", name: "翻译", url: "http://translate.google.cn", description: "0", tag_id: 23},{create_date: "2017-12-21T20:49:01Z", name: "虫部落", url: "http://www.chongbuluo.com", description: "0", tag_id: 23},{create_date: "2017-12-21T20:49:20Z", name: "盘搜", url: "http://www.panc.cc", description: "0", tag_id: 23},{create_date: "2017-12-21T20:49:35Z", name: "feed", url: "https://theoldreader.com", description: "0", tag_id: 23}]
+    $scope.labs = '';
+    $scope.marks = '';
     var apiurl = '/get_tags/';
     console.log(apiurl);
+
+    $scope.colorr = {
+         0 : "primary",
+         1 : "success",
+         2 : "info",
+         3 : "warning",
+         4 : "danger",
+         5 : "default",
+     }
 
     $scope.active_tag = "常用";
 
@@ -103,3 +112,11 @@ app.controller('addmarksController', ['$scope', '$modalInstance', '$http', '$win
         };
     };
 }]);
+
+
+app.filter('random_clr', function() {
+    return function (clr_obj){
+        var r = Math.round(Math.random()*5)
+        return clr_obj[r];
+    };
+});
