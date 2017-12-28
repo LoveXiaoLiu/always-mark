f0d1314521012f33dbb83b6855994a5765958faf
grass <shuai_grass@foxmail.com>
2017-12-28 15:39:55 +0800

    自动补全

--- src/web/src/indexCon.js
+++ src/web/src/indexCon.js
@@ -2,7 +2,7 @@
 * @Author: caoshuai
 * @Date:   2017-09-23 14:05:32
 * @Last Modified by:   anchen
-* @Last Modified time: 2017-12-28 14:45:20
+* @Last Modified time: 2017-12-28 15:39:17
 */
 
 var app = angular.module('myApp', ['ui.bootstrap']);
