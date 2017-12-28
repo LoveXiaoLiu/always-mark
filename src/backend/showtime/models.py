fde723ceca7c28b8c459775a8ab135b5a44f0bc2
grass <shuai_grass@foxmail.com>
2017-12-28 21:06:21 +0800

    tag 整理

--- src/backend/showtime/models.py
+++ src/backend/showtime/models.py
@@ -13,7 +13,7 @@ class MarkTag(models.Model):
     create_date = models.DateTimeField(auto_now_add=True)
     access_times = models.IntegerField(max_length=20, default=0)
     description = models.TextField()
-    parent_id = models.ForeignKey(MarkTag)
+    parent_id = models.ForeignKey('self')
 
 
 class UrlDetail(models.Model):
